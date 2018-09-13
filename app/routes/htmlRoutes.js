bacc6dd729e219bbf662fa31d129c9a4ac18d6cb
Johnny Zavala <jzavalafgz@gmail.com>
2018-09-13 17:26:57 -0400

    updated htmlRoutes.js to include dependencies, and routes

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,3 +1,30 @@
 // Your htmlRoutes.js file should include two routes:
 // A GET Route to /survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
+
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+
+module.exports = function (app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an html page of content
+    // ---------------------------------------------------------------------------
+
+    app.get("/tables", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    // if no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
